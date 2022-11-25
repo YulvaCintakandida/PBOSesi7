1be645bffdbc00aa81ba323beb1b2074ac8d7086
YulvaCintakandida <114393228+YulvaCintakandida@users.noreply.github.com>
2022-11-25 13:29:19 +0700

    Add files via upload

---
+++ Outer.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Outer;
+
+/**
+ *
+ * @author HP 14S
+ */
+public class Outer {
+    int number = 0;
+
+    private class Inner {
+        public void print() {
+            System.out.println("Mengakses inner class yang ke: " + (++number));
+        }
+    }
+
+    void displayFromMethod() {
+        Inner in = new Inner();
+        in.print();
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Outer out = new Outer();
+        out.displayFromMethod();
+        out.displayFromMethod();
+    }
+}
